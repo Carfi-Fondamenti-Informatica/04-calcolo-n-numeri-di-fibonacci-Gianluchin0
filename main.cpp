8f324db9e6647e628b5b637f7b9f161190e1c623
Gianluchin0 <118599894+Gianluchin0@users.noreply.github.com>
2022-11-22 16:41:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 
 int main() {
-   return 0;
+    int n, a=0, b=1, c=0;
+    cout << "Inserire un numero intero" << endl;
+    cin >> n;
+    if (n >= 2) {
+        cout << b << endl;
+     for (int i=0; i<n; i++) {
+        c = a+b;
+        a = b;
+        b =c;
+        cout << c << endl;
+     }
+    }
+    else {
+        cout << "errore";
+    }
+    return 0;
 }
